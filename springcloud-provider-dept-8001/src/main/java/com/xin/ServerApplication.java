baffd86bf3f35bfa26dfad653ffc71bdf4d983b7
孔鑫 <1737165855@qq.com>
2022-04-08 16:30:41 +0800

    Eureka服务注册与发现

--- springcloud-provider-dept-8001/src/main/java/com/xin/ServerApplication.java
+++ springcloud-provider-dept-8001/src/main/java/com/xin/ServerApplication.java
@@ -2,11 +2,14 @@ package com.xin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author KX
  * @date 2022/4/7 16:36
  */
+//在服务启动后自动注册到Eureka中
+@EnableEurekaClient
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
